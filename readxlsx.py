39d6590bc1a9d4258c5068c341e7ec61bbb11b14
unknown <sapphire.micare@gmail.com>
2022-08-06 23:38:34 +0900

    [init] read xlsx file

---
+++ readxlsx.py
@@ -0,0 +1,57 @@
+import openpyxl
+
+class readxlsx:
+    def __init__(self, bookname: str, sheetname: str) -> None:
+        self.wb = openpyxl.load_workbook(bookname)
+        self.ws = self.wb[sheetname]
+        print("[System]", "xlsx file read")
+
+    # private functions
+    def __get_rowrange(self, col: int) -> int:
+        row = 1
+        keep = 1050000
+        indata = True
+        while True:
+            sts = self.ws.cell(row=row, column=col).value is None
+            # print("keep:", keep, "row:", row, "indata:", indata, "sts:", sts)
+            
+            if indata ^ sts:
+                if keep > row:
+                    keep = row
+                else:
+                    return keep, row - 1
+
+            indata = sts
+            row += 1
+    
+    def __getgen_bycol(self, s_col: int, e_col:int) -> tuple:
+        s_row, e_row = self.__get_rowrange(s_col)
+        return self.ws.iter_rows(min_row=s_row, max_row=e_row, min_col=s_col, max_col=e_col)
+    
+    # public functions
+    def get_valuelist_bycol(self, s_col: int, e_col:int) -> list:
+        t_2d = self.__getgen_bycol(s_col, e_col)
+        return ([[cell.value for cell in row] for row in t_2d])
+    
+    def gen_linkdict(self, l_2d: list) -> dict:
+        link = {}
+        if len(l_2d[0]) != 2:
+            print("[Error]", "does not match size")
+            return None
+        
+        for l_val in l_2d:
+            if l_val[0] not in link:
+                link[l_val[0]] = [l_val[1]]
+            else:
+                link[l_val[0]].append(l_val[1])
+        
+        return link
+
+
+
+if __name__ == "__main__":
+    
+    FILENAME = "jobconnection.xlsx"
+    x = readxlsx(FILENAME, "Sheet1")
+    a = x.get_valuelist_bycol(1, 2)
+    print(x.gen_linkdict(a))
