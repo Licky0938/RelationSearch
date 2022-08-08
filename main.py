048b9097470f22f028427374e85266895e6ec91d
unknown <sapphire.micare@gmail.com>
2022-08-09 00:37:51 +0900

    [del] delete unnecessary import

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from tkinter.tix import TCL_IDLE_EVENTS
 from readxlsx import readxlsx as rxlx
 import RelationSearch as rels
 
@@ -14,16 +13,18 @@ dict_master = {}
 
 for key in dict_link:
     dict_master[key] = rels.relationship(key)
+    # print(key)
+a = dict_master["I"]
+a.add_link("forward", "J", dict_master["J"])
 
-for key in sorted(dict_link.keys()):
-    rels.link_forward(dict_master[key], dict_master, dict_link)
+# for key in sorted(dict_link.keys()):
+#     rels.link_forward(dict_master[key], dict_master, dict_link)
 
-set_other = set(dict_master.keys()) - set(t_lst)
-for key in dict_master:
-    d = {}
-    rels.get_flink(dict_master[key], d, -1)
-    print(dict_master[key], d)
-
+# set_other = set(dict_master.keys()) - set(t_lst)
+# for key in dict_master:
+#     d = {}
+#     rels.get_flink(dict_master[key], d, -1)
+#     print(dict_master[key], d)
 
 # for key in sorted(dict_master.keys()):
 #     print(dict_master[key].get_myname(), list(dict_master[key].get_forkeys()), list(dict_master[key].get_backkeys()))
