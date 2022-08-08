c557410b7a666924ab674e73a5c28d0216449b64
unknown <sapphire.micare@gmail.com>
2022-08-09 00:33:23 +0900

    [mod] unify forward and backward links

--- RelationSearch.py
+++ RelationSearch.py
@@ -1,4 +1,5 @@
 from inspect import getfile
+from turtle import back
 
 
 class relationship:
@@ -7,6 +8,7 @@ class relationship:
         self.__myname = myname
         self.__forward  = {}    # name:instance dictionary
         self.__backward = {}    # name:instance dictionary
+        self.__link = {"forward": {}, "backward": {}}
     
     def __str__(self) -> str:
         return self.__myname  #, self.depth, self.__fname, 
@@ -20,6 +22,9 @@ class relationship:
         if key not in self.__backward:
             self.__backward[key] = ins
     
+    def add_link(self, direction: str, key: str, ins: object) -> None:
+        self.__link[direction][key] = ins
+    
     # get members
     def get_myname(self) -> str:
         return self.__myname
@@ -30,32 +35,45 @@ class relationship:
     def get_backward(self) -> dict:
         return self.__backward
     
+    def get_link(self, direction: str) -> dict:
+        return self.__link[direction]
+    
+    def get_linkall(self) -> dict:
+        return self.__link
+
     def get_forkeys(self) -> list:
         return self.__forward.keys()
     
     def get_backkeys(self) -> list:
         return self.__backward.keys()
+    
+    def get_keys(self, direction: str) -> list:
+        return self.__link[direction].keys()
+    
+    def get_waykeys(self) -> list:
+        return self.__link.keys()
 
 # counter = 0
 
 def add_links(forward: relationship, backward: relationship) -> None:
-    forward.add_backward(backward.get_myname(), backward)
-    backward.add_forward(forward.get_myname(), forward)
+#     forward.add_backward(backward.get_myname(), backward)
+#     backward.add_forward(forward.get_myname(), forward)
+    forward.add_link("backward", backward.get_myname(), backward)
+    backward.add_link("forward",  forward.get_myname(),  forward)
 
 def get_flength(ins: relationship) -> int:
     return len(ins.get_forkeys())
 
+def get_elmtlength(direction: str, ins: relationship) -> int:
+    return len(ins.get_link(direction))
+
 def link_forward(rel: relationship, d_link: dict, d_name: dict) -> None:
-    # global counter
     for elmt in d_name[rel.get_myname()]:
-        # counter += 1
         if elmt not in d_link:
-            # print("in not")
             d_link[elmt] = relationship(elmt)
         add_links(forward=d_link[elmt], backward=rel)
         # print("me:", rel.get_myname(), "for:", d_link[elmt].get_myname())
-        if elmt not in d_name or len(d_name[elmt]) == get_flength(d_link[elmt]):
-            # print("already exist")
+        if elmt not in d_name or len(d_name[elmt]) == get_elmtlength("forward", d_link[elmt]):
             continue
         else:
             link_forward(d_link[elmt], d_link, d_name)
@@ -69,6 +87,17 @@ def get_flink(rel: relationship, d_depth: dict, depth: int) -> None:
         else:
             d_depth[key] = depth
         get_flink(d_fwd[key], d_depth, depth - 1)
+
+def get_relation(direction: str, rel: relationship, d_depth: dict, depth: int, mode: int = 1) -> None:
+    d_link = rel.get_link(direction)
+    for key in d_link.keys():
+        if key in d_depth:
+            if abs(d_depth[key]) * mode < abs(depth) * mode:
+                d_depth[key] = depth
+            continue
+        else:
+            d_depth[key] = depth
+        get_relation(direction, d_link[key], d_depth, depth, mode)
 
 if __name__ == "__main__":
     from readxlsx import readxlsx
