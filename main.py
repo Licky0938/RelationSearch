f9ad454c75afaa91307064b4f7331a76c062ff46
unknown <sapphire.micare@gmail.com>
2022-09-15 23:11:03 +0900

    [add] debug methods

--- main.py
+++ main.py
@@ -1,34 +1,56 @@
 from readxlsx import readxlsx as rxlx
 from RelationSearch import relationship as rels
-from RelationSearch import horizon as hrzn
+from RelationSearch import horizontal as hztl
 from RelationSearch import vertical as vtcl
-from RelationSearch import FORWARD, BACKWARD, HORI, VERT, ORDER, V_SORT
+from RelationSearch import FORWARD, BACKWARD, HZTL, VTCL, ORDER, V_SORT
+
+import time
 
 sourcefile = "jobconnection.xlsx"
 targetfile = "jobtarget.xlsx"
 
 
-s_xlx = rxlx(sourcefile, 0)
-t_xlx = rxlx(targetfile, 0)
-t_lst = t_xlx.get_targetset(1)
+xl_source = rxlx(sourcefile)
+xl_target = rxlx(targetfile)
+s_tname = xl_target.get_targetset(1)
 
 # dictionary[ownname] = [jobname0, jobname1, jobname2, ...]
-dict_link = s_xlx.get_link(1, 2)
+d_forward = xl_source.get_link(1, 2)
 # dictionary[ownname] = rels.relationship(ownname)
-dict_master = {}
+d_relship = {}
 
-for key in dict_link:
-    dict_master[key] = rels(key)
+for key in d_forward:
+    d_relship[key] = rels(key)
 
-for key in dict_link:
-    hrzn.link_horizon(dict_master[key], dict_master, dict_link)
+num = 1
+mlen = len(d_forward)
+time_begin = time.time()
+for key in d_forward:
+    time_s = time.time()
+    hztl.link_horizon(d_relship[key], d_relship, d_forward)
+    print("[System]", "time:", f"{time.time() - time_s: .3f}", "horizontal process:", num, "/", mlen, end='\r')
+    num +=1
+print("\n[System]", "Horizontal time:", f"{time.time() - time_begin: .3f}")
 
-for elmt in dict_master:
-    vtcl.get_vertical(dict_master[elmt])
+num = 1
+mlen = len(d_relship)
+time_begin = time.time()
+for elmt in d_relship:
+    time_s = time.time()
+    vtcl.get_vertical(d_relship[elmt])
+    print("[System]", "time:", f"{time.time() - time_s: .3f}", "vretical process:", num, "/", mlen, end='\r')
+    num +=1
+print("\n[System]", "Vertical time:", f"{time.time() - time_begin: .3f}")
 
 with open("test.csv", mode="w") as f:
-    for key in dict_master:
-        f.write(key + ',' + ','.join(dict_master[key].get_link(FORWARD,  ORDER)[V_SORT]) + '\n')
-        f.write(      ',' + ','.join(dict_master[key].get_link(BACKWARD, ORDER)[V_SORT]) + '\n')
+    for key in sorted(["XA", "XD"]):
+        f.write(key + ',' + ','.join(sorted(d_relship[key].get_link(FORWARD,  ORDER)[V_SORT])) + '\n')
+        f.write(      ',' + ','.join(sorted(d_relship[key].get_link(BACKWARD, ORDER)[V_SORT])) + '\n')
+print("[System]", "test file is generated")
 
-set_extract = set(dict_master.keys()) - t_lst
+# set_extract = set(s_tname)
+# print(s_tname)
+# for key in set(d_relship.keys()) - set_extract:
+#     if len(set(d_relship[key].get_link(FORWARD, HZTL).keys()) & s_tname) > 0:
+#         set_extract.add(key)
+#         print(key)
